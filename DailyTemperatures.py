a7ef33a6a2ecf6165adcdfc2a227b9918eb45a59
Gautam-Balachandran <gautam.balachandran@gmail.com>
2024-07-29 09:18:10 -0400

    Stack-1 Completed

---
+++ DailyTemperatures.py
@@ -0,0 +1,32 @@
+﻿# Time Complexity : O(n), where is the length of the temperatures list
+# Space Complexity : O(n)
+
+from typing import List
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans = [0] * len(temperatures)
+        stack = []  # This will be the decreasing stack
+        
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                index = stack.pop()
+                ans[index] = i - index
+            stack.append(i)
+        
+        return ans
+
+# Example 1
+temperatures1 = [73, 74, 75, 71, 69, 72, 76, 73]
+solution1 = Solution()
+print(solution1.dailyTemperatures(temperatures1))  # returns [1, 1, 4, 2, 1, 1, 0, 0]
+
+# Example 2
+temperatures2 = [30, 40, 50, 60]
+solution2 = Solution()
+print(solution2.dailyTemperatures(temperatures2))  # returns [1, 1, 1, 0]
+
+# Example 3
+temperatures3 = [30, 60, 90]
+solution3 = Solution()
+print(solution3.dailyTemperatures(temperatures3))  # returns [1, 1, 0]
---
+++ NextGreaterElement2.py
@@ -0,0 +1,40 @@
+﻿# Time Complexity : O(n)
+# Space Complexity : O(n)
+from typing import List
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return nums
+        n = len(nums)
+        result = [-1] * n  # Fill the result with -1, which is the default value
+        stack = []
+
+        for i in range(n * 2):  # Since it is a circular array, we are iterating till n*2
+            while stack and nums[stack[-1]] < nums[i % n]:
+                result[stack.pop()] = nums[i % n]
+            if i < n:
+                stack.append(i)
+
+        return result
+
+# Examples
+sol = Solution()
+
+# Example 1
+nums1 = [1, 2, 1]
+print("Example 1: nums =", nums1)
+print("Output:", sol.nextGreaterElements(nums1))
+# Expected Output: [2, -1, 2]
+
+# Example 2
+nums2 = [3, 8, 4, 1, 2]
+print("Example 2: nums =", nums2)
+print("Output:", sol.nextGreaterElements(nums2))
+# Expected Output: [8, -1, 8, 2, 3]
+
+# Example 3
+nums3 = [6, 5, 4, 3, 2, 1]
+print("Example 3: nums =", nums3)
+print("Output:", sol.nextGreaterElements(nums3))
+# Expected Output: [-1, 6, 6, 6, 6, 6]
